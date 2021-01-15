36f59c5da2d36b7e40df0552efb412a3ea6c9e1c
ravi0497 <muhammadravi04@gmail.com>
2021-01-16 01:39:14 +0700

    front end detail

--- blog_project/views.py
+++ blog_project/views.py
@@ -17,7 +17,7 @@ class AboutView(TemplateView):
 
     def get_context_data(self, **kwargs):
          context = super().get_context_data(**kwargs)
-         context['now'] = Now.objects.get(pk=1)
+         context['now'] = Now.objects.get(pk=2)
          return context
 
 
